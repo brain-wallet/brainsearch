28b71fed01b006622b5e6337626b047319daa5bb
Melvin Carvalho <melvincarvalho@gmail.com>
2024-02-21 16:39:37 +0100

    full alphabet

--- bin/search.js
+++ bin/search.js
@@ -14,9 +14,9 @@ var end = 'd'
 var search = [secret] // adbcdd
 
 
-var ascii = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var ascii = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&']
 const len = ascii.length
-const chars = 5
+const chars = 4
 
 
 function generateCyhper(i, chars, ascii, len) {
